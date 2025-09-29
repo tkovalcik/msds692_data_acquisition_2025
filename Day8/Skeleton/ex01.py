fba17f507d05daa477fb2a34c6563c5e3e342cce
tkovalcik <tomasko.kovalcik@gmail.com>
2025-09-29 13:01:00 -0700

    up to sept 29

--- Day8/Skeleton/ex01.py
+++ Day8/Skeleton/ex01.py
@@ -1,8 +1,31 @@
 from playwright.sync_api import sync_playwright
+import time
 
+with sync_playwright() as p:
 # Step 1. Create a browser
 # Can use chromium/firefox/webkit
+    browser = p.chromium.launch(headless=False)
 
 # Step 2. Create a new BrowserContext
+    context = browser.new_context()
+    page = context.new_page()
+    # time.sleep(1)
 
 # Step 3. Open a page
+    page.goto("https://reddit.com")
+    # time.sleep(1)
+
+    # page.wait_for_selector("summary")
+    # page.wait_for_selector("#grecaptcha-badge")
+    page.wait_for_selector("main")
+    page.query_selector_all("a")
+
+    for anchor in page.query_selector_all("a"):
+        # print(anchor.inner_html())
+        print(anchor.get_attribute("href"))
+
+    # time.sleep(2)
+
+    # print(page.title())
+
+    browser.close()
--- Day8/Skeleton/ex03.py
+++ Day8/Skeleton/ex03.py
@@ -10,7 +10,10 @@ with sync_playwright() as p:
 
     # Waiting for Elements
     page.wait_for_selector("main")  # Wait Until the <main> appears
-
+    time.sleep(1)
     # Click an anchor with a text, "About Reddit"
-
+    page.locator("a", has_text="About Reddit").click()
+    time.sleep(1)
     # Wheel to go 150 pxl, 1000 pxl
+    page.mouse.wheel(150, 1000)
+    time.sleep(1)
